f690ae0f4221c346fb3857e47d305fd52a2d5284
Unknown Decoder <155003129+UnknownDecoder@users.noreply.github.com>
2025-06-20 17:22:06 +0530

    Add files via upload

---
+++ DataPack.py
@@ -0,0 +1,57 @@
+class DataPack:
+    
+    class Conversations:
+        def __init__(self):
+            self.data = {
+                
+                "hello": "Hey there!",
+                "hi": "Hello!",
+                "hey": "Hi there!",
+                "yo": "What's up?",
+                "good morning": "Good morning to you!",
+                "good afternoon": "Good afternoon!",
+                "good evening": "Good evening!",
+                "howdy": "Howdy! How can I help you?",
+                "namaste": "Namaste! 🙏",
+                "hola": "Hola amigo!",
+                "bonjour": "Bonjour! Well… I don't know French that much 😅",
+                
+                "how are you": "I'm just a bot, but I'm doing great!",
+                "how are you doing": "I'm functioning as expected!",
+                "what's up": "Just waiting for your message!",
+                "hru": "All good, thanks! What about you?",
+                "sup": "All good here!",
+                "how’s it going": "Going smooth, thanks!"
+            }
+    
+    class ThanksAndBye:
+        def __init__(self):
+            self.data = {
+                "thank you": "You're welcome!",
+                "thanks": "No problem!",
+                "thanks a lot": "Happy to help!",
+                "bye": "Goodbye! Take care!",
+                "see you": "See you soon!",
+                "later": "Catch you later!"
+            }
+    
+    class FamousPeople:
+        def __init__(self):
+            self.data = {
+                "who is elon musk": "CEO of Tesla and SpaceX. Also plays with rockets 😅",
+                "who is bill gates": "Co-founder of Microsoft and now a philanthropist.",
+                "who is narendra modi": "Prime Minister of India.",
+                "who is albert einstein": "One of the greatest physicists who ever lived.",
+                "who is taylor swift": "A popular American singer-songwriter."
+            }
+    
+    class Brands:
+        def __init__(self):
+            self.data = {
+                "what is apple": "A tech company known for the iPhone and Mac.",
+                "what is samsung": "A South Korean company making phones, TVs, and more.",
+                "what is google": "The world's biggest search engine, and more.",
+                "what is microsoft": "Creators of Windows, Office, and Xbox.",
+                "what is starlink": "Elon Musk's satellite internet service."
+            }
+            
