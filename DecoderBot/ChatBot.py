d6713d04de561969d62a7f4e5cf46282a4c020cf
Unknown Decoder <155003129+UnknownDecoder@users.noreply.github.com>
2025-07-04 21:36:45 +0530

    Update ChatBot.py

--- DecoderBot/ChatBot.py
+++ DecoderBot/ChatBot.py
@@ -16,15 +16,14 @@ class ChatBot:
         self._imp_responses  = {
             "name" : self.nameResponse
         }
-        
         self.threshold = threshold
         
         self._responses.update(self._imp_responses )
 
-    def add_response(self, message, reply):
+    def train_for(self, message, reply):
         self._responses[message.lower()] = reply.capitalize()
 
-    def set_responses(self, responses):
+    def set_training(self, responses):
         if not isinstance(responses, dict):
             raise self.ResponseTypeError("a dictionary value must be passed through set_responses")
         self._responses = {}
@@ -34,7 +33,7 @@ class ChatBot:
         
         self._responses.update(self._imp_responses )
     
-    def add_responses_dict(self, responses):
+    def train(self, responses):
         if not isinstance(responses, dict):
             raise self.ResponseTypeError("a dictionary value must be passed through add_responses_dict")
         
@@ -47,8 +46,22 @@ class ChatBot:
         a = difflib.get_close_matches(message.lower(), self._responses.keys(), n=1, cutoff=self.threshold)
         return self._responses.get(a[0]) if a else m
     
-    
-    
-    def clear_responses(self):
+    def reset_training(self):
         self._responses = {}
+        self._responses.update(self._imp_responses)
+
+    def reset_bot(self, name="My Chatbot", threshold=0.2):
+        self.name = name
+        self.nameResponse = f"My name is {self.name}"
+        self._responses = {
+            "hello" : "Hey there, how are you?",
+            "bye" : "Goodbye! You can come here whenever you want."
+        }
+        
+        self._imp_responses  = {
+            "name" : self.nameResponse
+        }
+        
+        self.threshold = threshold
+        
         self._responses.update(self._imp_responses)
