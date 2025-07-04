d45a361d93c76d1b20ac338459139370d8e7a5c0
Unknown Decoder <155003129+UnknownDecoder@users.noreply.github.com>
2025-07-04 21:21:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DecoderBot',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     author='Unknown Decoder',
     description='A simple trainable chatbot using OOP in Python',
